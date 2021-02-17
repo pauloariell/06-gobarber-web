985760c7f757567beddef132ec9e1d0fcb5322a7
pauloariell <pauloariell@live.com>
2021-02-17 00:07:00 -0300

    GlobalStyle

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
 
-const App: React.FC = () => <h1>Teste caray</h1>;
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => (
+  <>
+    <h1>Teste caray</h1>
+    <GlobalStyle />
+  </>
+);
 
 export default App;
