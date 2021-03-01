2e49c5ade5e02572af14fdc0cfae122b79097542
pauloariell <pauloariell@live.com>
2021-02-28 23:24:48 -0300

    Página de login

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import GlobalStyle from './styles/global';
+import SignIn from './pages/SignIn/index';
 
 const App: React.FC = () => (
   <>
-    <h1>Teste caray</h1>
     <GlobalStyle />
+    <SignIn />
   </>
 );
 
